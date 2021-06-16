4484415b2a73c74456081417e09bdb32715a9bff
Nao-RandD <nao.r.and.d@gmail.com>
2021-06-16 20:04:48 +0900

    tableViewを追加

--- AppDojoTask13/AppDojoTask13/ViewController.swift
+++ AppDojoTask13/AppDojoTask13/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let fruits = ["りんご", "みかん", "バナナ", "パイナップル"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+               numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = fruits[indexPath.row]
+        return cell
+    }
+}
